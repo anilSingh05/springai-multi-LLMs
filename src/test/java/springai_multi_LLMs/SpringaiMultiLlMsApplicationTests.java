b54479bfd186b3e0822fc79abf3579b08083a860
Anil Singh <anil05.me@gmail.com>
2025-01-18 02:05:23 -0600

    Initial Commit

---
+++ src/test/java/springai_multi_LLMs/SpringaiMultiLlMsApplicationTests.java
@@ -0,0 +1,13 @@
+package springai_multi_LLMs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringaiMultiLlMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
